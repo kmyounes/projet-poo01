b4e6be5b9945cc2436eb3663b67eecc1c0b72b9c
kmyounes <kmyounes@protonmail.com>
2017-02-16 21:11:23 +0100

    modifié :         bin/Ville.class modifié :         src/Ville.java

--- src/Ville.java
+++ src/Ville.java
@@ -1,8 +1,22 @@
+//char inChar = in.next().toLowerCase().charAt(0); 
+
 import java.util.Arrays;
 
 public class Ville {
+  //=====================Types=============================
+  private enum types{agricole,touristique,industrielle,ordinaire};
+ 
+  
 	
-//=====================Constructeurs========================
+	//=====================Variables========================
+	
+	private String nom,wilaya;
+	private double superficie,habitants;
+	private int nombreFleurs;
+	private Ville[] voisins;
+	private types type;
+
+  //=====================Constructeurs========================
 
 	public Ville(String nom, String wilaya, double superficie, double habitants) {
 		this.nom = nom;
@@ -10,57 +24,67 @@ public class Ville {
 		this.superficie = superficie;
 		this.habitants = habitants;
 	}	
+  
+	//=====================Geters/Seters========================
 	
-//variables	
-	private String nom,wilaya;
-	
-	private double superficie,habitants;
-	private String type;
-	private int nombreFleurs;
-	private Ville[] voisins;
 	public String getNom() {
 		return nom;
 	}
+	
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
 	public String getWilaya() {
 		return wilaya;
 	}
+	
 	public void setWilaya(String wilaya) {
 		this.wilaya = wilaya;
 	}
+	
 	public double getSuperficie() {
 		return superficie;
 	}
+	
 	public void setSuperficie(double superficie) {
 		this.superficie = superficie;
 	}
+	
 	public double getHabitants() {
 		return habitants;
 	}
+	
 	public void setHabitants(double habitants) {
 		this.habitants = habitants;
 	}
+	
 	public String getType() {
-		return type;
+		return type.name();
 	}
+	
 	public void setType(String type) {
-		this.type = type;
+		this.type = types.valueOf(type);
+		
 	}
+	
 	public int getNombreFleurs() {
 		return nombreFleurs;
 	}
+	
 	public void setNombreFleurs(int nombreFleurs) {
 		this.nombreFleurs = nombreFleurs;
 	}
+	
 	public Ville[] getVoisins() {
 		return voisins;
 	}
+	
 	public void setVoisins(Ville[] voisins) {
 		this.voisins = voisins;
 	}
 
+  
 	
 	@Override
 	public String toString() {
@@ -68,16 +92,16 @@ public class Ville {
 				+ ", type=" + type + ", nombreFleurs=" + nombreFleurs + ", voisins=" + Arrays.toString(voisins) + "]";
 	}
 	
-	//==============méthodes==============
+	//=====================Méthodes========================
 	
 	public void couleurAffiche(){
-		if(this.type=="ordinaire" &&( this.nombreFleurs<=1)){
+		if(this.type==types.valueOf("ordinaire") &&( this.nombreFleurs<=1)){
 		
 			System.out.println("\u001B[31m"+ this.toString()+ "\u001B[0m");
 			
 		}
-		else if ((this.type=="ordinaire" && this.nombreFleurs>0) || (this.type!="ordinaire" && this.nombreFleurs==0 ) ){
-		//afficher en orange	
+		else if ((this.type==types.valueOf("ordinaire") && this.nombreFleurs>0) || (this.type!=types.valueOf("ordinaire") && this.nombreFleurs==0 ) ){
+     		//afficher en orange	
 		}
 		else {
 			//afficher en vert
@@ -92,5 +116,22 @@ public class Ville {
 		return(this.voisins.length==3);
 	
 	}
+	
+	public boolean voisinDifferent(){
+for (Ville v : this.voisins){
+			if (this.type==v.type) { return false;}
+			
+		}
+		return true;
+	}
+	
+	public boolean plusFleurie(){
+		for (Ville v : this.voisins){
+			if (this.nombreFleurs<v.nombreFleurs) { return false;}
+			
+		}
+		return true;
+	}
+	
 
 }
