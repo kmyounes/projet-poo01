3a3987d88622c1c9d6f97e11c35e2bffbabf6b8d
Legend96th <sofmaro@hotmail.fr>
2017-02-19 21:11:06 +0100

    Added some stuff

---
+++ src/Application.java
@@ -0,0 +1,16 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Ville v = new Ville("Babez", "", 365.72, 10000);
+		v.setNombreFleurs(0);
+		v.setType("ordinaire");
+		
+		v.couleurAffiche();
+		
+		
+	}
+
+}
