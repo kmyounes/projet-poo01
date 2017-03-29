2120395f938725e400a9f1ed3720c3a30692cb63
kmyounes <kmyounes@protonmail.com>
2017-03-29 11:48:05 +0100

    arganized POO things

--- src/Application.java
+++ src/Application.java
@@ -47,179 +47,23 @@ public class Application {
 
 		
 		//afficheReseauVille(v);
-		afficheReseauArray(villes);
+		Ville.afficheReseauArray(villes);
 		
 		System.out.println();
 		System.out.println();
 		
-		afficherListeVilles(villes);
+		Ville.afficherListeVilles(villes);
 		
-		System.out.println(rechListe(villes, "Medeia"));
+		System.out.println(Ville.rechListe(villes, "Medeia"));
 		
 		//System.out.println(v.existeChemin(s));
 		
 		
 	}
 	
-	public static Ville rechListe(Collection<Ville> villes, String nom){
-		/*
-		for(Ville v : liste){
-			if(v.getNom().equals(nom))
-				return v;
-		}
-		*/
-		Ville temp;
-		
-		for(Ville v: villes){
-			temp = v.recherche(nom);
-			if(temp != null)
-				return temp;
-			
-		}
-		
-		return null;
-		
-	}
 	
 	
-	public static void modifieVille(Ville v){
-		int choice = 1;
-		Scanner in = new Scanner(System.in);
-		
-		do{
-			System.out.printf("What do you want to change?: %n 1) Nom 2) Wilaya 3) Superficie 4) Type de ville 0) Abort.%n Votre choix: ");
-			choice = in.nextInt();
-			
-			if(choice < 0 || choice > 4)
-				System.out.println("Erreur!! Please enter a valide choice!");
-			
-		}while(choice < 0 || choice > 4);
-			
-		switch(choice){
-		case 1: 
-			System.out.println("Enter the new name:  ");
-			v.setNom(in.next());
-			break;
-		case 2: 
-			System.out.println("Enter the new wilaya:  ");
-			v.setWilaya(in.next());
-			break;
-		case 3: 
-			System.out.println("Enter the new :  ");
-			v.setSuperficie(in.nextDouble());
-			break;
-		case 4: 
-			int type = 1;
-			do{
-				System.out.printf("Enter the new type:  1) Agricole, 2) Touristique, 3) Industrielle, 4) Ordinaire.%n Votre choix: ");
-				type = in.nextInt();
-				if(type < 1 || type >4)
-					System.out.println("Error!! Enter a valid type!");
-			}while(type < 1 || type >4);
-			
-			switch(type){
-			case 1:
-				v.setType("agricole");
-				break;
-			case 2:
-				v.setType("touristique");
-				break;
-			case 3:
-				v.setType("industrielle");
-				break;	
-			case 4:
-				v.setType("ordinaire");
-				break;
-			}
-			break;
-		}
-		
-		System.out.println("Done!");
-		in.close();
-	}
 	
-	static public boolean supprime(ArrayList<Ville> liste, Ville v){
-		
-		if(liste.indexOf(v) == -1){
-			System.out.println("Ville n'existe pas dans la liste!");
-			return false;
-		}
-				
-		liste.remove(v);
-		System.out.println("Operation terminee");
-		return true;
-		
-		
-	}
-	
-	public static void afficheReseauArray(ArrayList<Ville> liste){
-			
-		for(Ville v: liste){
-			afficheReseauVille(v, 0, 0);
-		}
-	}
-	
-	public static void afficheReseauVille(Ville v){
-		afficheReseauVille(v, 0, 0);
-	}
-	
-	public static void afficheReseauVille(Ville v, int numSpace, int i){
-		
-		System.out.print(v.getNom());
-		
-		numSpace += v.getNom().length();
-		//numSpace += 5;
-				
-		if( ! v.pasChemin()){
-			
-			boolean space = false;
-			
-			//for(Ville temp : v.getVoisinsSortants()){
-			for(Ville temp : v.getVoisSort()){
-				
-				if(temp != null){
-					if(space){
-						
-						//make a string of ' ' numSpaces time.
-						char[] chars = new char[ numSpace + i*4 ];
-						Arrays.fill(chars, ' ');
-						String result = new String(chars);
-						System.out.print(result);
-						
-						
-					} else {
-						space = !space;
-						
-					}
-					
-					System.out.print(" => ");
-					
-					afficheReseauVille(temp, numSpace ,i+1);
-					
-				}
-			}
-		}
-		
-		System.out.println();
-	}
-	
-	public static void afficherListeVilles(ArrayList<Ville> liste){
-		String vert = "", rouge = "", orange= "";
-		
-		for(Ville v: liste){
-			if(v.getCouleur().equalsIgnoreCase("vert"))
-				vert = vert + " " + v.getNom();			
-			if(v.getCouleur().equalsIgnoreCase("rouge"))
-				rouge = rouge + " " + v.getNom();
-			if(v.getCouleur().equalsIgnoreCase("orange"))
-				orange = orange + " " + v.getNom();
-		}
-		
-		System.out.println("La liste des villes: ");
-		System.out.println("Vert:  " + vert);
-		System.out.println("Rouge:  " + rouge);
-		System.out.println("Orange:  " + orange);
-		
-	}
-
 }
+	
+	
